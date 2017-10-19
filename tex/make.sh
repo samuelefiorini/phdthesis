de5797bd8abec13a464d3c95d06cd8281645133c
Samuele Fiorini <samuele.fiorini@gmail.com>
2017-10-19 10:51:08 +0200

    utility script updated

--- tex/make.sh
+++ tex/make.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c SIGINT
+
+function ctrl_c() {
+        echo "** LaTeX compilation stopped... **"
+	exit -1
+}
+
 mkdir ./_build
-pdflatex -output-directory=./_build main.tex
-bibtex main.aux
-pdflatex -output-directory=./_build main.tex
-pdflatex -output-directory=./_build main.tex
-pdflatex -output-directory=./_build main.tex
-pdflatex -output-directory=./_build main.tex
-mv ./_build/main.pdf .
+pdflatex -file-line-error -halt-on-error -output-directory=./_build main.tex
+bibtex ./_build/main.aux
+
+for i in `seq 1 2`; do
+	pdflatex -file-line-error -halt-on-error -output-directory=./_build main.tex > /dev/null
+        echo -n "."
+done
+echo
+
--- tex/pdfclean.sh
+++ tex/pdfclean.sh
@@ -1,5 +1,19 @@
+# Clean the folder
 rm -rf _build
 rm *.aux
 rm *.log
 rm *.out
 rm *.toc
+rm *.fls
+rm *.brf
+rm *.synctex.gz
+rm *.bbl
+rm *.blg
+rm *.lof
+rm *.lot
+rm *.fdb_latexmk
+
+# Prepare for the next build
+mkdir _build
+touch _build/main.pdf
+ln -s _build/main.pdf main.pdf
