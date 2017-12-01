c9d89130afc849e9923edaefbf03d47124195b97
Samuele Fiorini <samuele.fiorini@gmail.com>
2017-12-01 16:33:52 +0100

    adenine chapter expanded

--- src/data/AISM/config.py
+++ src/data/AISM/config.py
@@ -5,7 +5,7 @@
 from adenine.utils import data_source
 
 # --------------------------  EXPERMIENT INFO ------------------------- #
-exp_tag = '_experiment'
+exp_tag = '_small'
 output_root_folder = 'results'
 plotting_context = 'notebook'  # one of {paper, notebook, talk, poster}
 file_format = 'pdf'  # or 'png'
@@ -13,51 +13,50 @@ use_compression = False  # use gzip to compress the results
 
 # ----------------------------  INPUT DATA ---------------------------- #
 # Load an example dataset or specify your input data in tabular format
-data_file = 'data.csv'
-labels_file = 'labels.csv'  # OPTIONAL
+data_file = 'anonimyzed_data_03-2017.csv'
+labels_file = 'anonimyzed_labels_03-2017.csv'  # OPTIONAL
 samples_on = 'rows'  # if samples lie on columns use 'cols' or 'col'
 data_sep = ','  # the data separator. e.g., ',', '\t', ' ', ...
 X, y, feat_names, index = data_source.load('custom',
                                            data_file, labels_file,
                                            samples_on=samples_on,
+#                                           n_samples=150,
                                            sep=data_sep)
 
 # -----------------------  PIPELINES DEFINITION ------------------------ #
 # --- Missing values imputing --- #
-step0 = {'Impute': [False, {'missing_values': 'NaN',
-                            'strategy': ['median',
-                                         'mean',
-                                         'nearest_neighbors']}]}
+step0 = {'Impute': [True, {'missing_values': 'NaN',
+                            'strategy': ['median']}]}
 
 # --- Data preprocessing --- #
-step1 = {'None': [False], 'Recenter': [False], 'Standardize': [False],
+step1 = {'None': [False], 'Recenter': [False], 'Standardize': [True],
          'Normalize': [False, {'norm': ['l1', 'l2']}],
-         'MinMax': [False, {'feature_range': [(0, 1), (-1, 1)]}]}
+         'MinMax': [True, {'feature_range': [(0, 1)]}]}
 
 # --- Unsupervised features learning --- #
 # affinity ca be precumputed for SE
-step2 = {'PCA': [False, {'n_components': 3}],
+step2 = {'PCA': [True, {'n_components': 3}],
          'IncrementalPCA': [False],
          'RandomizedPCA': [False],
          'KernelPCA': [False, {'kernel': ['linear', 'rbf', 'poly']}],
-         'Isomap': [False, {'n_neighbors': 5}],
+         'Isomap': [True, {'n_neighbors': 5}],
          'LLE': [False, {'n_neighbors': 5,
                          'method': ['standard', 'modified',
                                     'hessian', 'ltsa']}],
          'SE': [False, {'affinity': ['nearest_neighbors', 'rbf']}],
          'MDS': [False, {'metric': True}],
-         'tSNE': [False],
+         'tSNE': [False, {'perplexity': [100]}],
          'RBM': [False, {'n_components': 256}],
          'None': [False]
          }
 
 # --- Clustering --- #
 # affinity ca be precumputed for AP, Spectral and Hierarchical
-step3 = {'KMeans': [False, {'n_clusters': [3, 'auto']}],
+step3 = {'KMeans': [True, {'n_clusters': [2, 'auto']}],
          'AP': [False, {'preference': ['auto']}],
          'MS': [False],
-         'Spectral': [False, {'n_clusters': [3, 8]}],
-         'Hierarchical': [False, {'n_clusters': [3, 8],
-                                  'affinity': ['manhattan', 'euclidean'],
-                                  'linkage':  ['ward', 'complete', 'average']}]
+         'Spectral': [False, {'n_clusters': [3]}],
+         'Hierarchical': [True, {'n_clusters': [3],
+                                  'affinity': ['euclidean'],
+                                  'linkage':  ['ward']}]
          }
