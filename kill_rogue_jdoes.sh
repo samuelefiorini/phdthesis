69330a2434d23bc926c188cc5da4152b75244157
Samuele Fiorini <samuele.fiorini@gmail.com>
2018-01-02 10:39:33 +0100

    pco description completed

--- kill_rogue_jdoes.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kill -9 `ps aux | grep samu | grep python | grep stability_selection | awk '{print $2}'` 2> /dev/null
-echo "Output of ps aux | grep jdoe | grep pd_run :"
-ps aux | grep samu | grep stability_selection
